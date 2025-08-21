79d275f0bee6cc3a9b7f6d2b085e917d2ab8d934
MohamedAlawakey <mohamedelawakey@gmail.com>
2025-08-21 18:25:12 +0300

    2. Implementing User Follows and Feed Functionality (edit 2)

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -40,27 +40,24 @@ class ProfileView(generics.RetrieveUpdateAPIView):
     def get_object(self):
         return self.request.user
 
-@api_view(["POST"])
-@permission_classes([IsAuthenticated])
-def follow_user(request, user_id):
-    user_to_follow = get_object_or_404(CustomUser, id=user_id)
-    request.user.following.add(user_to_follow)
-    return Response({"message": f"You are now following {user_to_follow.username}"},
-                    status=status.HTTP_200_OK)
+class FollowUserView(generics.GenericAPIView):
+    permission_classes = [IsAuthenticated]
 
+    def post(self, request, user_id):
+        user_to_follow = get_object_or_404(CustomUser, id=user_id)
+        request.user.following.add(user_to_follow)
+        return Response(
+            {"message": f"You are now following {user_to_follow.username}"},
+            status=status.HTTP_200_OK
+    )
 
-@api_view(["POST"])
-@permission_classes([IsAuthenticated])
-def unfollow_user(request, user_id):
-    user_to_unfollow = get_object_or_404(CustomUser, id=user_id)
-    request.user.following.remove(user_to_unfollow)
-    return Response({"message": f"You have unfollowed {user_to_unfollow.username}"},
-                    status=status.HTTP_200_OK)
+class UnfollowUserView(generics.GenericAPIView):
+    permission_classes = [IsAuthenticated]
 
-
-@api_view(["GET"])
-@permission_classes([IsAuthenticated])
-def list_users(request):
-    users = CustomUser.objects.all()  
-    data = [{"id": user.id, "username": user.username} for user in users]
-    return Response(data, status=status.HTTP_200_OK)
+    def post(self, request, user_id):
+        user_to_unfollow = get_object_or_404(CustomUser, id=user_id)
+        request.user.following.remove(user_to_unfollow)
+        return Response(
+            {"message": f"You have unfollowed {user_to_unfollow.username}"},
+            status=status.HTTP_200_OK
+    )
